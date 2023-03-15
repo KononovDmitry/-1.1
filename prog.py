cceff7b9fde28f4813c79dc899d5121a7daf822a
KononovDmitry <dkononov2903@gmail.com>
2023-03-15 13:25:09 +0300

    add print and this end this program

--- prog.py
+++ prog.py
@@ -4,4 +4,4 @@ for x in range(2):
         for z in range(2):
             for w in range(2):
                 if ((not(y <= x)) or (z <= w) or (not(z))) == False:
-                    
+                    print(x, y, z, w)
